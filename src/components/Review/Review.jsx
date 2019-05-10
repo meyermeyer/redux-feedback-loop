b96d25efa75b179e924302190cb57a3f51cc7954
Meyer <ehlmeyer@gmail.com>
2019-05-10 18:33:45 -0500

    moved submit button from form to review component, imported connect

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import { Button } from '@material-ui/core';
+import { connect } from 'react-redux';
 
 class Review extends Component {
     render() {
         return (
-            <h2>Review</h2>
+            <>
+                <h2>Review</h2>
+                <Button variant="contained" color="secondary" disabled>Submit</Button>
+            </>
         )
     }
 }
 
-export default Review;
+const mapReduxStateToProps = (reduxState) => {
+    return {
+        reduxState
+    }
+}
+
+export default connect(mapReduxStateToProps)(Review);
