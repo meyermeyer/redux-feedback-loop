59ae7059670dc02531a2d8844176be029033f918
Meyer <ehlmeyer@gmail.com>
2019-05-10 18:52:28 -0500

    updated comments form with some material UI

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import TextField from '@material-ui/core/TextField';
 
 class Comments extends Component {
+    handleChange = (event) => {
+        console.log('in handleChange');
+        // this.setState ({
+        //     feeling: event.target.value
+        // })
+        this.props.dispatch({ type: 'UPDATE_FEEDBACK', payload: event.target.value, name: 'comments' })
+    }
+
     render() {
         return (
-            <h2>Comments!</h2>
+            <>
+                <h2>Comments!</h2>
+                <TextField
+                    label="Comments"
+                    variant="outlined"
+                    id="custom-css-outlined-input"
+                    onChange={this.handleChange}
+                />
+            </>
+            
+            // <>
+            //     <h2>Comments!</h2>
+            //     <input type="text" placeholder="comments"/>
+            // </>
+            
         )
     }
 }
 
-export default Comments;
+
+
+export default connect()(Comments);
