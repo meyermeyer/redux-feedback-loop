884104acb1d01dd762f0ada830682f7d745dacca
Meyer <ehlmeyer@gmail.com>
2019-05-10 18:30:17 -0500

    support form works

--- src/components/Support/Support.jsx
+++ src/components/Support/Support.jsx
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
+import { Button } from '@material-ui/core';
+import { connect } from 'react-redux';
 
 class Support extends Component {
+    handleChange = (event) => {
+        console.log('in handleChange');
+        // this.setState ({
+        //     feeling: event.target.value
+        // })
+        this.props.dispatch({ type: 'UPDATE_FEEDBACK', payload: event.target.value, name: 'support' })
+    } 
+
     render() {
         return (
-            <h2>Support!</h2>
+            <>
+                <h2>Support!</h2>
+                <select onChange={this.handleChange} name="support">
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                </select>
+                <Button onClick={this.handleClick} variant="contained" color="primary">Next</Button>
+            </>
         )
     }
 }
 
-export default Support;
+export default connect()(Support);
