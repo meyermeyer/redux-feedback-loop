e17037bfacba6fc5a30f2aea99aaf95f902c2491
Meyer <ehlmeyer@gmail.com>
2019-05-10 17:31:51 -0500

    components created and import

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
+import { HashRouter as Router, Link, Route } from 'react-router-dom';
+import Form from '../Form/Form';
+import Success from '../Success/Success';
 
 class App extends Component {
   render() {
@@ -9,6 +12,8 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Feedback!</h1>
           <h4><i>Don't forget it!</i></h4>
+          <Form />
+          <Success />
         </header>
         <br/>
       </div>
