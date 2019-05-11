8a42c361c27f9c25aa932418ac7b204cd7da5e8f
Meyer <ehlmeyer@gmail.com>
2019-05-11 08:56:19 -0500

    success page created and button sends back to home

--- src/components/Success/Success.jsx
+++ src/components/Success/Success.jsx
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
+import {Button} from '@material-ui/core'
 // import {HashRouter, withRouter} from 'react-router-dom'
 
 class Success extends Component {
+
+    handleClick = ()=> {
+        this.props.history.push('/');
+    }
     render () {
         return(
-            <h2>Success!</h2>
+            <>
+                <h2>Success!</h2>
+
+                <Button onClick={this.handleClick} variant="contained" color="secondary">Leave New Feedback</Button>
+            </>
+            
         )
     }
 }
