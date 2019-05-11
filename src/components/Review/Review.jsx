e1599598b4884346596497636e658a228240ca91
Meyer <ehlmeyer@gmail.com>
2019-05-11 08:51:26 -0500

    client side post request works

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Button } from '@material-ui/core';
 import { connect } from 'react-redux';
+import axios from 'axios';
 // import { HashRouter as Router, Link, Route } from 'react-router-dom';
 import Success from '../Success/Success'
 
@@ -9,15 +10,19 @@ class Review extends Component {
 
     handleClick = () => {
         console.log('in handleClick-review');
-        this.props.history('/success')
-    
-    }
+        // this.props.history('/success')
+        axios.post('/feedback', this.props.feedback)
+        .then(response => {
+            console.log('back from POST /feedback', response);
+        })//end .then
+        .catch(error => {
+            console.log('error in POST /feedback', error);
+        })//end .catch
+    };// end handleClick
 
     render() {
-        console.log(this.props.history);
+        console.log(this.props.feedback);
         
-        
-
         return (
             <>
                 {/* <Router>
