de620874fdba069a08c8145740093df04034abb7
Meyer <ehlmeyer@gmail.com>
2019-05-11 09:07:40 -0500

    added material ui card to Review component

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button } from '@material-ui/core';
+import { Button, Card, CardContent, CardActions, Typography  } from '@material-ui/core';
 import { connect } from 'react-redux';
 import axios from 'axios';
 // import { HashRouter as Router, Link, Route } from 'react-router-dom';
@@ -28,15 +28,39 @@ class Review extends Component {
                 {/* <Router>
                     <Route exact path='/success' component={Success} />
                 </Router> */}
-                
-                <h2>Review</h2>
+                <Card >
+                    <CardContent>
+                        {/* <Typography className={classes.title} color="textSecondary" gutterBottom>
+                        Word of the Day
+                        </Typography> */}
+                        <Typography variant="h5" component="h2">
+                        Review Your Feedback
+                        </Typography>
+                        <Typography>
+                        Feeling: {this.props.feedback.feeling}
+                        </Typography>
+                        <Typography component="p">
+                        Understanding: {this.props.feedback.understanding}
+                        </Typography>
+                        <Typography>
+                            Support: {this.props.feedback.support}
+                        </Typography>
+                        <Typography>
+                            Comments: {this.props.feedback.comments}
+                        </Typography>
+                    </CardContent>
+                    <CardActions style={{ justifyContent: 'center' }}>
+                        <Button onClick={this.handleClick} variant="contained" color="secondary" disabled={!this.props.isFilled}>Submit</Button>
+                    </CardActions>
+                    </Card>
+                {/* <h2>Review</h2>
                 <ul>
                     <li>Feeling: {this.props.feedback.feeling}</li>
                     <li>Understanding: {this.props.feedback.understanding}</li>
                     <li>Support: {this.props.feedback.support}</li>
                     <li>Comments: {this.props.feedback.comments}</li>
                 </ul>
-                <Button onClick={this.handleClick} variant="contained" color="secondary" disabled={!this.props.isFilled}>Submit</Button>
+                <Button onClick={this.handleClick} variant="contained" color="secondary" disabled={!this.props.isFilled}>Submit</Button> */}
             </>
         )
     }
