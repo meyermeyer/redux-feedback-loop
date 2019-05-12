a58814d51c2c6288cfb086b6427d1a5fbea7f88f
Meyer <ehlmeyer@gmail.com>
2019-05-11 22:06:52 -0500

    add new feedback button has add icon

--- src/components/Success/Success.jsx
+++ src/components/Success/Success.jsx
@@ -16,7 +16,7 @@ class Success extends Component {
                 <p><i class="material-icons" id="check-circle" >check_circle_black_72x72</i></p>
                 <h2>Success!</h2>
 
-                <Button onClick={this.handleClick} variant="contained" color="secondary">Leave New Feedback</Button>
+                <Button onClick={this.handleClick} variant="contained" color="secondary"><i class="material-icons" id="add-icon" >add_black_72x72</i>Leave New Feedback</Button>
             </>
             
         )
